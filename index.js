0f80ce1aab3b832d6aa55326ade86a44522896fc
Isaac <gr25.main@gmail.com>
2022-06-19 22:56:25 +0100

    added privacy policy

---
+++ index.js
@@ -0,0 +1,7 @@
+import * as slideShow from './source/scripts/slide-show.js';
+import * as ghostie from './source/scripts/ghostie.js';
+
+window.onload = () => {
+    slideShow.start();
+    ghostie.start();
+};
