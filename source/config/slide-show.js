0bc7cee43334b8547a1e83b3604ba41978abb065
Isaac <gr25.main@gmail.com>
2022-06-19 23:36:36 +0100

    file structure update

--- source/config/slide-show.js
+++
@@ -1,9 +0,0 @@
-import { createSrc, createAlt } from '../scripts/common.js';
-
-export const slideShow = [
-    // maze-game
-    { alt: createAlt('MAZE GAME', 'QUADRANT'), src: createSrc('maze-game/quadrant.png') },
-    { alt: createAlt('MAZE GAME', 'CORRIDOR'), src: createSrc('maze-game/corridor.png') },
-    { alt: createAlt('MAZE GAME', 'CIRCULAR'), src: createSrc('maze-game/circular.png') },
-    { alt: createAlt('MAZE GAME', 'PASSAGE'), src: createSrc('maze-game/passage.png') },
-];
--- source/scripts/slide-show.js
+++ source/scripts/slide-show.js
@@ -1,12 +1,20 @@
-import { slideShow } from '../config/slide-show.js';
+import { createAlt, createSrc } from './common.js';
 
-let i = 0;
+const slideShow = [
+    // maze-game
+    { alt: createAlt('MAZE GAME', 'QUADRANT'), src: createSrc('maze-game/quadrant.png') },
+    { alt: createAlt('MAZE GAME', 'CORRIDOR'), src: createSrc('maze-game/corridor.png') },
+    { alt: createAlt('MAZE GAME', 'CIRCULAR'), src: createSrc('maze-game/circular.png') },
+    { alt: createAlt('MAZE GAME', 'PASSAGE'), src: createSrc('maze-game/passage.png') },
+];
+
+let index = 0;
 
 const update = () => {
-    document.getElementById('slide-show').setAttribute('src', slideShow[i].src);
-    document.getElementById('slide-show').setAttribute('alt', slideShow[i].alt);
-    document.getElementById('slide-show').setAttribute('title', slideShow[i].alt);
-    i = i === slideShow.length - 1 ? 0 : i + 1;
+    document.getElementById('slide-show').setAttribute('src', slideShow[index].src);
+    document.getElementById('slide-show').setAttribute('alt', slideShow[index].alt);
+    document.getElementById('slide-show').setAttribute('title', slideShow[index].alt);
+    index = index === slideShow.length - 1 ? 0 : index + 1;
     change();
 };
 
