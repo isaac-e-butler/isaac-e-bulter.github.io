ea14a535186cc466da8020f649579c9a7165cade
Isaac <gr25.main@gmail.com>
2022-12-29 00:48:23 +0000

    minor tweaks

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
+import GenerateSocialMedia from './source/scripts/social-media-generate.js';
 import StartAutoSearch from './source/scripts/project-auto-search.js';
-import socialList from './source/scripts/social-media.js';
 
 window.onload = () => {
     GenerateSocialMedia();
     StartAutoSearch();
 };
 
-function GenerateSocialMedia() {
-    socialList.forEach(s =>
-        document.getElementById('social-media').innerHTML += `
-            <a href="${s.link}" target="_blank">
-                <img 
-                    src="./source/images/social-media/${s.icon}" 
-                    alt="${s.alt}" 
-                    draggable="false"
-                />
-            </a>
-    `);
-}
---
+++ source/scripts/social-media-generate.js
@@ -0,0 +1,14 @@
+import list from "./social-media-list.js";
+
+export default function() {
+    list.forEach(s =>
+        document.getElementById('social-media').innerHTML += `
+            <a href="${s.link}" target="_blank">
+                <img 
+                    src="./source/images/social-media/${s.icon}" 
+                    alt="${s.alt}" 
+                    draggable="false"
+                />
+            </a>
+    `);
+}
