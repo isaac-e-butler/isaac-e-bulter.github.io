2d8b8400fcbb867a2c8e2df4a8148647f4bd07ea
Isaac <gr25.main@gmail.com>
2023-01-09 04:39:07 +0000

    project page within home

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import GenerateSocialMedia from './source/scripts/social-media-generate.js';
 import StartAutoSearch from './source/scripts/project-auto-search.js';
 
 window.onload = () => {
-    GenerateSocialMedia('./source');
-    StartAutoSearch(8, { source: './source', project: './projects/' });
+    GenerateSocialMedia();
+    StartAutoSearch();
 };
