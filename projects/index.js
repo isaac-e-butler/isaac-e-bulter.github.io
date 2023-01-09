6accf18af407012af32a7cc2b831fe4768277cab
Isaac <30571202+GhostRunners25@users.noreply.github.com>
2023-01-09 04:42:20 +0000

    Website Version 4.1 (#3)

--- projects/index.js
+++
@@ -1,7 +0,0 @@
-import GenerateSocialMedia from '../source/scripts/social-media-generate.js';
-import StartAutoSearch from '../source/scripts/project-auto-search.js';
-
-window.onload = () => {
-    GenerateSocialMedia('../source');
-    StartAutoSearch(32, { source: '../source', project: './' });
-};
